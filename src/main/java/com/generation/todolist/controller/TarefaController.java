9a24372c54c4a3e3f720f5b07524bd9e10da3ae0
“fbuba987” <115090534+fbuba987@users.noreply.github.com>
2022-12-09 22:03:47 -0300

    Commitando os testes

---
+++ src/main/java/com/generation/todolist/controller/TarefaController.java
@@ -0,0 +1,39 @@
+package com.generation.todolist.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.todolist.model.Tarefa;
+import com.generation.todolist.repository.TarefaRepository;
+
+@RestController
+@RequestMapping("/tarefas")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class TarefaController {
+
+    @Autowired
+    private TarefaRepository tarefaRepository;
+
+    @PostMapping
+    public ResponseEntity<Tarefa> post(@Valid @RequestBody Tarefa tarefa){
+        return ResponseEntity.status(HttpStatus.CREATED).body(tarefaRepository.save(tarefa));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tarefa> getById(@PathVariable Long id) {
+        return tarefaRepository.findById(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+}
---
+++ src/test/java/com/generation/todolist/controller/TarefaControllerTest.java
@@ -0,0 +1,69 @@
+package com.generation.todolist.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.generation.todolist.model.Tarefa;
+import com.generation.todolist.repository.TarefaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TarefaControllerTest {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Autowired
+    private TarefaRepository tarefaRepository;
+
+    @Test
+    @DisplayName("Criar nova Tarefa")
+    public void deveCriarNovaTarefa() throws Exception {
+
+        Tarefa tarefa = new Tarefa(0L, "Tarefa 01", "Tarefa numero 1", "João", LocalDate.now(), true);
+
+        HttpEntity<Tarefa> corpoRequisicao = new HttpEntity<Tarefa>(tarefa);
+
+        ResponseEntity<Tarefa> resposta = testRestTemplate
+                .exchange("/tarefas", HttpMethod.POST, corpoRequisicao, Tarefa.class);
+
+        assertEquals(HttpStatus.CREATED, resposta.getStatusCode());
+        assertEquals(corpoRequisicao.getBody().getNome(), resposta.getBody().getNome());
+
+    }
+
+    @Test
+    @DisplayName("Listar uma Tarefa Específica")
+    public void deveListarApenasUmaTarefa() {
+
+        Tarefa buscaTarefa = tarefaRepository.save(new Tarefa(0L, "Tarefa 02", "Tarefa numero 2", "Maria", LocalDate.now(), true));
+
+        ResponseEntity<String> resposta = testRestTemplate
+                .exchange("/tarefas/" + buscaTarefa.getId(), HttpMethod.GET, null, String.class);
+
+        assertEquals(HttpStatus.OK, resposta.getStatusCode());
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tarefa> getById(@PathVariable Long id) {
+        return tarefaRepository.findById(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
